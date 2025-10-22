7d55f449d81d0e56ea90e531978c227f85f59663
Lonie <12333holbertonstudents.com>
2025-10-23 01:07:44 +0200

    delete space

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Point d’entrée
  *
- * Return: Always 0 (Success)
+ * Return: Toujours 0 (Succès)
  */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
-return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
