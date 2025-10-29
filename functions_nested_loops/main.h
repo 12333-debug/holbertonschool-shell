4ff74b32c0eb4f2d11716cf953fe5efd285d60d2
Lonie <12333@holbertonstudents.com>
2025-10-29 10:20:55 +0100

    Write a program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int_putchar(char c);
+
+#endif /* MAIN_H */#ifndef MAIN_H
+#define MAIN_H
+
+int_putchar(char c);
+
+#endif /* MAIN_H */#ifndef MAIN_H
+#define MAIN_H
+
+int_putchar(char c);
+
+#endif /* MAIN_H */
